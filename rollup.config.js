3e7a79a45b6bf9ec889305b6e9ef4cad4262462b
lesenelir <hi@lesenelir.me>
2023-09-27 16:00:22 +0800

    feat: resolve + common plugins

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,8 @@
 // pnpm rollup --config rollup.dev.config.js / pnpm rollup -c rollup.prod.config.js
 
 import json from 'rollup-plugin-json'
+import resolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
 
 export default {
   input: "src/index.js",
@@ -10,5 +12,5 @@ export default {
     dir: "dist",
     format: "esm"
   },
-  plugins: [json()]
+  plugins: [commonjs(), json(), resolve()]
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import {login} from './login.js'
 import {message} from './message.js'
+import answer from 'the-answer'
+import moment from 'moment'
 
 import json from '../data/data.json' assert { type: 'json' }
 
@@ -8,4 +10,7 @@ message()
 
 login(json.name)
 login(json.author)
+
+console.log('the answer is ' + answer)
+console.log(moment().format('MMMM Do YYYY, h:mm:ss a'))
 
